860a8acfe4098b194cbd4a1c6fc97c2c130fe0c3
SttlUser <sttluser@gmail.com>
2023-06-26 15:27:17 +0530

    Delete api parameter sequence changed

--- Angular/src/app/customer.service.ts
+++ Angular/src/app/customer.service.ts
@@ -28,7 +28,8 @@ export class CustomerService {
 
   public DeletUserData(num:any,id:any){
     console.log(num,id);
-    const usr = [id, num]
+    // const usr = [id, num]
+    const usr = [num, id]
     return this.http.post(this.Dispaly_url+'UserMaster/DeleteUserMaster',usr);
   }
 
