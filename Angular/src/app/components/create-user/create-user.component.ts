ca039fe83172fcffa2f9c8d8a7f7ba9b63e8658a
neerajsc1901 <neeraj.ssttl@outlook.com>
2023-07-25 19:29:30 +0530

    -- create user  (value = default) -- rolemaster validation

--- Angular/src/app/components/create-user/create-user.component.ts
+++ Angular/src/app/components/create-user/create-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import {CustomerService} from '../../customer.service'
+import { CustomerService } from '../../customer.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environment';
@@ -11,19 +11,25 @@ import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.css'],
 })
 export class CreateUserComponent {
   isLoggedIn: boolean = true;
-  message = "USER CREATED SUCCESSFULLY";
-  items:any=[];
-  url:string=`${environment.api.server}/RoleMater/GetRoleMasterData?id=`+1;
+  message = 'USER CREATED SUCCESSFULLY';
+  items: any = [];
+  url: string = `${environment.api.server}/RoleMater/GetRoleMasterData?id=` + 1;
   snackBar: any;
-  constructor(private resto:CustomerService, private router: Router ,public http:HttpClient,private toastService: ToastService,@Inject(MAT_SNACK_BAR_DATA) public data: any){
+  constructor(
+    private resto: CustomerService,
+    private router: Router,
+    public http: HttpClient,
+    private toastService: ToastService,
+    @Inject(MAT_SNACK_BAR_DATA) public data: any
+  ) {
     this.Getdata(http);
   }
   applyForm = new FormGroup({
-    cb_pk_id:new FormControl('1'),
+    cb_pk_id: new FormControl('1'),
     firstname: new FormControl(''),
     middlename: new FormControl(''),
     lastname: new FormControl(''),
@@ -31,16 +37,16 @@ export class CreateUserComponent {
     password: new FormControl(''),
     email: new FormControl(''),
     phone: new FormControl(''),
-    ins_del_id: new FormControl('90'),  
+    ins_del_id: new FormControl('Default'),
   });
 
-  Getdata(http:HttpClient){
+  Getdata(http: HttpClient) {
     http.get(this.url).subscribe((res: any) => {
       this.items = res;
-      return this.items
-    })
-}
-  PostUserData(){
+      return this.items;
+    });
+  }
+  PostUserData() {
     const formData = { ...this.applyForm.value };
     const {
       firstname,
@@ -50,9 +56,8 @@ export class CreateUserComponent {
       password,
       email,
       phone,
-      ins_del_id
-      
-    } = formData; 
+      ins_del_id,
+    } = formData;
     if (
       !firstname ||
       !middlename ||
@@ -61,71 +66,79 @@ export class CreateUserComponent {
       !password ||
       !email ||
       !phone ||
-      !ins_del_id 
-      
-    ){
+      !ins_del_id
+    ) {
       console.log('Please fill in all fields.');
       alert('Please fill in all fields.');
       return;
     }
     if (!formData.firstname) {
-      alert("Firstname is required");
+      alert('Firstname is required');
       return;
     }
-  
-     if (!formData.lastname) {
-      alert("Lastname is required");
+
+    if (!formData.lastname) {
+      alert('Lastname is required');
       return;
     }
-    if (!isCapitalized(formData.firstname) || !isCapitalized(formData.lastname) ) {
-      alert("First name and last name must be in capitalized format");
+    if (
+      !isCapitalized(formData.firstname) ||
+      !isCapitalized(formData.lastname)
+    ) {
+      alert('First name and last name must be in capitalized format');
       return;
     }
     function isCapitalized(value: string) {
       // Check if the first character of the value is capitalized
       return /^[A-Z]/.test(value);
     }
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
-if (!passwordRegex.test(password)) {
-  console.log('Password must contain at least 8 characters including at least one uppercase letter, one lowercase letter, one digit, and one special character.');
-  alert('Password must contain at least 8 characters including at least one uppercase letter, one lowercase letter, one digit, and one special character.');
-  return;
-}
-
-// Validate the email format
-const emailRegex = /^[A-Za-z0-9._%+-]+@(gmail|yahoo|outlook|hotmail)\.(com|edu|org|net|gov|co)$/;
-if (!emailRegex.test(email)) {
-  console.log('Invalid email format.');
-  alert('Invalid email format.');
-  return;
-}
-const phoneRegex = /^\d{10}$/;
-if (!phoneRegex.test(phone)) {
-  console.log('Invalid phone number format. Please enter a 10-digit phone number.');
-  alert('Invalid phone number format. Please enter a 10-digit phone number.');
-  return;
-}
-    console.log('Details entered',formData);
-    this.resto.PostUserData(formData).subscribe(
-    (res)=>{
-    console.log(res);  
-    this.toastService.recieve(this.message);
-    this.router.navigate(['/UserMaster']);
-    },
-    (err)=>{
-      console.log(err);
-      const message = 'Something went wrong.';
-      this.snackBar.openFromComponent(ToastComponent, {
-        data: { message },
-        duration: 2000, // Toast duration in milliseconds
-        horizontalPosition: 'end',
-        verticalPosition: 'top'
-      });
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+    if (!passwordRegex.test(password)) {
+      console.log(
+        'Password must contain at least 8 characters including at least one uppercase letter, one lowercase letter, one digit, and one special character.'
+      );
+      alert(
+        'Password must contain at least 8 characters including at least one uppercase letter, one lowercase letter, one digit, and one special character.'
+      );
+      return;
     }
-    )
-    
+
+    // Validate the email format
+    const emailRegex =
+      /^[A-Za-z0-9._%+-]+@(gmail|yahoo|outlook|hotmail)\.(com|edu|org|net|gov|co)$/;
+    if (!emailRegex.test(email)) {
+      console.log('Invalid email format.');
+      alert('Invalid email format.');
+      return;
+    }
+    const phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phone)) {
+      console.log(
+        'Invalid phone number format. Please enter a 10-digit phone number.'
+      );
+      alert(
+        'Invalid phone number format. Please enter a 10-digit phone number.'
+      );
+      return;
+    }
+    console.log('Details entered', formData);
+    this.resto.PostUserData(formData).subscribe(
+      (res) => {
+        console.log(res);
+        this.toastService.recieve(this.message);
+        this.router.navigate(['/UserMaster']);
+      },
+      (err) => {
+        console.log(err);
+        const message = 'Something went wrong.';
+        this.snackBar.openFromComponent(ToastComponent, {
+          data: { message },
+          duration: 2000, // Toast duration in milliseconds
+          horizontalPosition: 'end',
+          verticalPosition: 'top',
+        });
+      }
+    );
   }
-
 }
-
-
